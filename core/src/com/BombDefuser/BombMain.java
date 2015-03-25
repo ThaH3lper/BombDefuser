9dfa5f615404685874258d9852f34d1497ce4c5c
ThaH3lper <craftopia@live.com>
2015-03-25 12:35:34 +0100

    Added Gameobjekt *Changed default clear color to white

--- core/src/com/BombDefuser/BombMain.java
+++ core/src/com/BombDefuser/BombMain.java
@@ -49,7 +49,7 @@ public class BombMain extends ApplicationAdapter {
 	public void render () {
 		update();
 		
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		stateManager.render();
---
+++ core/src/com/BombDefuser/Utilities/GameObject.java
@@ -0,0 +1,134 @@
+package com.BombDefuser.Utilities;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class GameObject {
+	
+	private Texture tex;
+	protected Vector2 pos, origin;
+	protected float width, height, scale, scaleX, scaleY, rotation;
+	protected Rectangle source, hitbox;
+	protected Color color;
+	
+	public GameObject(Texture tex){
+		this(tex, 0, 0, tex.getWidth(), tex.getHeight(), 0, 0, tex.getWidth(), tex.getHeight(), Color.WHITE);
+	}
+	public GameObject(Texture tex, float sourceX, float sourceY, float sourceWidth, float sourceHeight, float x, float y, float width, float height, Color color){
+		this.tex = tex;
+		this.width = width;
+		this.height = height;
+		this.source = new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight);
+		this.hitbox = new Rectangle(x, y, width, height);
+		this.pos = new Vector2(x, y);
+		this.origin = new Vector2(width/2, height/2);
+		this.rotation = 0f;
+		this.scaleX = 1f;
+		this.scaleY = 1f;
+		this.color = color;
+	}
+	
+	public void render(SpriteBatch batch){
+		batch.setColor(color);
+		//debug code
+		//batch.draw(tex, hitbox.x, hitbox.y, hitbox.width, hitbox.height);
+		batch.draw(tex, pos.x, pos.y, origin.x, origin.y, width, height, scaleX, scaleY, rotation, (int)source.x, (int)source.y, (int)source.width, (int)source.height, false, false);
+		batch.setColor(Color.WHITE);
+	}
+	
+	public Rectangle getSource(){
+		return source;
+	}
+	
+	public Rectangle getHitbox(){
+		return hitbox;
+	}
+	
+	public Vector2 getCenterPosition(){
+		return new Vector2(pos.x + origin.x, pos.y + origin.y);
+	}
+	
+	public void updateHitBox(){
+		hitbox = new Rectangle(pos.x, pos.y, hitbox.width, hitbox.height);
+	}
+	
+	public Vector2 getPos() {
+		return pos;
+	}
+
+	public void setPos(Vector2 pos) {
+		this.pos = pos;
+	}
+
+	public Vector2 getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(Vector2 origin) {
+		this.origin = origin;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public void setWidth(float width) {
+		this.width = width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public float getScale() {
+		return scale;
+	}
+
+	public void setScale(float scale) {
+		this.scale = scale;
+	}
+
+	public float getRotation() {
+		return rotation;
+	}
+
+	public void setRotation(float rotation) {
+		this.rotation = rotation;
+	}
+
+	public void setSource(Rectangle source) {
+		this.source = source;
+	}
+
+	public void setHitbox(Rectangle hitbox) {
+		this.hitbox = hitbox;
+	}
+
+	public float getScaleX() {
+		return scaleX;
+	}
+
+	public void setScaleX(float scaleX) {
+		this.scaleX = scaleX;
+	}
+
+	public float getScaleY() {
+		return scaleY;
+	}
+
+	public void setScaleY(float scaleY) {
+		this.scaleY = scaleY;
+	}
+	
+	public void setColor(Color color){
+		this.color = color;
+	}
+	
+}
